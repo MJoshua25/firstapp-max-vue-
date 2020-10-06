fdb8b761f28e15f3e7af251ecb6a98d84a951151
Josué Yao <jyao2000@yahoo.fr>
2020-10-06 09:52:19 +0000

    create template, data, method

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,18 @@
 <template>
-
+	<p>Server Status: {{ status }}</p>
 </template>
 
 <script>
 export default {
   name: "Home",
   data: function (){
+  	return {
+  		status: 'Critical'
+	  };
+  },
+	methods: {
 
-  }
+	}
 }
 </script>
 
