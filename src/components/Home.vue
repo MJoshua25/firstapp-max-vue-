734e10eccf13fd4622497d75f8c7242ba40c5c28
Josué Yao <jyao2000@yahoo.fr>
2020-10-06 10:02:15 +0000

    use component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,9 @@
 <template>
-	<p>Server Status: {{ status }}</p>
-	<hr>
-	<button @click="changeStatus">Change Status</button>
+	<div>
+		<p>Server Status: {{ status }}</p>
+		<hr>
+		<button @click="changeStatus">Change Status</button>
+	</div>
 </template>
 
 <script>
